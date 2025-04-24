f323f6a870e91532f127f27c57ee23e4be41f153
Aashwin Garg <aashwinxgarg@gmail.com>
2025-04-25 01:20:44 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+console.log("Hitesh");
+
+
+let name = "Hitesh";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
